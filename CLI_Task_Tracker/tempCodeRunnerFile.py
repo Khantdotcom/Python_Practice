e2c392c1f0e5d8ecef091c6ba2d277b5cfc79653
Khantdotcom <kokhanthtay@gmail.com>
2025-09-27 16:55:48 +0700

    Fixed

---
+++ CLI_Task_Tracker/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+'''main.py'''
+from handle_input import InputHandling
+from load_tasks import LoadDatabase
+from engine import DatabaseFunction
+
+stop = False
+while not stop:
+    database = LoadDatabase()
+    read_data = database.entry_point()
+    input = InputHandling(read_data)
+    cleaned_input = input.valid_command()
+    stop = input.go_on()
+    engine = DatabaseFunction(read_data,cleaned_input)
+
+    engine.match_command()
+
+    update_database = engine.updated_database()
+
+    database.update(update_database)
